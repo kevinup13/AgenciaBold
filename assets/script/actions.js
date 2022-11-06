6a7d8ed0aff6c5b23cd3dedde6d588747525e614
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-06 01:39:12 -0400

    projeto ms eng

---
+++ assets/script/actions.js
@@ -0,0 +1,13 @@
+var menuBtn = document.querySelector('.items-menu--mobile i');
+menuBtn.addEventListener('click', () => {
+    let itemsMenu = document.querySelector('.menu-mobile');
+    if (itemsMenu.classList.contains('show')) {
+        itemsMenu.classList.remove('show');
+        itemsMenu.classList.add('hide');
+    } else {
+        itemsMenu.classList.remove('hide');
+        itemsMenu.classList.add('show');
+    }
+
+
+});
